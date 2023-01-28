be1c15c8eff2a682a34753dbf57b089ba628ada0
agustin <agupalavecino97@gmail.com>
2023-01-27 21:40:09 -0300

    change api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    name: 'dev',
-    url: 'http://localhost:3000/api/'
-    // url: 'https://gestory-api.herokuapp.com/api/',
+    production: true,
+    name: 'prod',
+    // url: 'http://localhost:3000/api/'
+    url: 'https://planner-api-kbw6.onrender.com/api/',
   };
